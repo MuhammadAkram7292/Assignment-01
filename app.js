199783c262963ee5781a33a8c272892bf2824894
MuhammadAkram7292 <121758916+MuhammadAkram7292@users.noreply.github.com>
2023-01-26 00:28:20 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//CHAPTER NO:01
+
+//Question:01
+// alert("Hello, Sir")
+
+//Question:02
+// alert("Error! Please enter a valid pasword.")
+
+//Question:03
+// alert("Welcome to JS Land... \n Happy Coding! ")
+
+//Question:04
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n Prevent this page creating additional dialogs.")
+
+//Question:05
+// alert("Hello... I can run JS through my web browser console")
+
+//Question:06
+// alert("Hey, I'm Cortana... \n How can I help you!")
+
+//Question:07
